03cb9bc9c33a38e93550f624b9a819f03ebb6afc
Ankur DalsaniYa <dalsaniaankur@gmail.com>
2018-02-19 17:13:36 +0530

    Upload Lumen API

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+    return [
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->LastName,
+        'email' => $faker->email,
+        'password' => \Illuminate\Support\Facades\Hash::make('pass'),
+        'city' => $faker->city,
+        'status' => rand(1,0)
+    ];
+});
